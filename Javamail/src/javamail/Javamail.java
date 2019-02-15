13df9c5c2c311c582760ec51b352b4ae411b0472
Patapoff <30930761+Patapoff@users.noreply.github.com>
2019-02-14 22:56:19 -0200

    Deletar email concluido

--- Javamail/src/javamail/Javamail.java
+++ Javamail/src/javamail/Javamail.java
@@ -5,6 +5,7 @@
  */
 package javamail;
 
+import java.util.Scanner;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -22,41 +23,58 @@ public class Javamail {
             // mail server connection parameters
        String host = "pop.gmail.com";
        String user = "littleheadfilms@gmail.com";
-       String password = "EmailLH12";
-
+       String senha = "EmailLH12";
+       Scanner leitor = new Scanner(System.in);
+       String resposta;
+       Folder inbox;
+       Store store;
        
        try
        {
-       // connect to my pop3 inbox
-       Properties properties = System.getProperties();
-       Session session = Session.getDefaultInstance(properties, null);
-       Store store = session.getStore("pop3s");
-       store.connect(host, user, password);
-       Folder inbox = store.getFolder("Inbox");
-       inbox.open(Folder.READ_ONLY);
+        for(;;)
+        {// connect to my pop3 inbox
+             Properties properties = System.getProperties();
+             Session session = Session.getDefaultInstance(properties, null);
+             store = session.getStore("pop3s");
+             store.connect(host, user, senha);
+             inbox = store.getFolder("Inbox");
+             inbox.open(Folder.READ_WRITE);
 
-       // get the list of inbox messages
-       Message[] messages = inbox.getMessages();
+             // get the list of inbox messages
+             Message[] mensagens = inbox.getMessages();
 
-       if (messages.length == 0) System.out.println("No messages found.");
+             if (mensagens.length == 0) System.out.println("Nenhuma mensagem encontrada.");
 
-       for (int i = 0; i < messages.length; i++) {
-         // stop after listing ten messages
-         if (i > 10) {
-           System.exit(0);
-           inbox.close(true);
-           store.close();
-         }
 
-         System.out.println("Message " + (i + 1));
-         System.out.println("From : " + messages[i].getFrom()[0]);
-         System.out.println("Subject : " + messages[i].getSubject());
-         System.out.println("Sent Date : " + messages[i].getSentDate());
-         System.out.println();
-       }
+             for (int i = 0; i < mensagens.length; i++) 
+             {
+                 // stop after listing ten messages
+                 if (i > 10) 
+                   break;
+                 
+                 System.out.println("Mensagem " + (i + 1));
+                 System.out.println("De : " + mensagens[i].getFrom()[0]);
+                 System.out.println("Assunto : " + mensagens[i].getSubject());
+                 System.out.println("Data de envio : " + mensagens[i].getSentDate());
+                 System.out.println();
+                 System.out.println("Quer deletar esta mensagem?[S/N]");
+                 resposta = leitor.nextLine();
+                 if(resposta.equals("S") || resposta.equals("s"))
+                 {
+                     Message mensagem = mensagens[i];
+                     mensagem.setFlag(Flags.Flag.DELETED, true);
+                     System.out.println("A mensagem foi deletada.");
+                 }
+             }
+             /*System.out.println("Deseja ver os seus emails de novo?[S/N]");
+             resposta = leitor.nextLine();
+             if(resposta.equals("N") || resposta.equals("n"))*/
+                break;
+        }
 
-       inbox.close(true);
-       store.close();
+       
+        inbox.close(true);
+        store.close();
      }
      catch(Exception e)
      {
